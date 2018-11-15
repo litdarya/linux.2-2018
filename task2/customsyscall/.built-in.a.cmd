054706dc4fefbc4d53a59def85c4aa5c91e33c2f
Darya Virtualbox <darya@vbox.vbox>
2018-11-15 17:22:50 +0300

    testing syscall

---
+++ task2/customsyscall/.built-in.a.cmd
@@ -0,0 +1,1 @@
+cmd_customsyscall/built-in.a := rm -f customsyscall/built-in.a; ar rcSTPD customsyscall/built-in.a customsyscall/sysgetuser.o
---
+++ task2/customsyscall/person.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#define MAX_NAME_LEN 30
+
+struct Person {
+    char name[MAX_NAME_LEN];
+    char surname[MAX_NAME_LEN];
+    char phone[MAX_NAME_LEN];
+    char email[MAX_NAME_LEN];
+
+    char age[MAX_NAME_LEN];
+};
+
